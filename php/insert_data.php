7ef594c2543de4d5af9bc588cc778486b8118378
Oluwajuwon-F <fawoleoluwajuwon@gmail.com>
2019-09-18 23:51:02 +0100

    php files

--- php/insert_data.php
+++ php/insert_data.php
@@ -1,26 +1,49 @@
 <?php
 
+require 'create_table.php';
+
 $servername = "localhost";
 $username = "username";
 $password = "password";
-$dbname = "webDB"
+$dbname = "webDB";
  
-/ Create connection
-$conn = new mysqli($servername, $username, $password);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 // Escape user inputs for security
-$email = mysqli_real_escape_string($link, $_REQUEST['email']);
-$password = mysqli_real_escape_string($link, $_REQUEST['password']);
- 
+if (isset($_POST['email']))
+{
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+}
+if (isset($_POST['password']))
+{
+$passwrd = mysqli_real_escape_string($conn, $_POST['password']);
+}
+if (isset($_POST['firstname']))
+{
+$firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+}
+if (isset($_POST['lastname']))
+{
+$lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+}
+/*
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+$firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+$lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+*/
+
 // Attempt insert query execution
-$sql = "INSERT INTO Users (email, password) VALUES ('$email', '$password')";
+$sql = "INSERT INTO Users (firstname, lastname, email, password) VALUES ('".$firstname."', '".$lastname."', '".$email."', '".$passwrd."')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+	readfile("../success.html");
+    //echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
